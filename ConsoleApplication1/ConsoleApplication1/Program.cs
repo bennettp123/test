5454ce9fa28179dbe0920aad716df94bedbaa43a
Bennett Perkins <Bennett.Perkins@education.wa.edu.au>
2015-12-07 10:02:39 +0800

    wat

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string wat = "wat?";
+            System.Console.Out.WriteLine(wat);
         }
     }
 }
