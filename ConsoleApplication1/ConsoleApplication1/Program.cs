a2fe7e40d9dd2440cca5fbbe01cabbf25add754b
Bennett Perkins <Bennett.Perkins@education.wa.edu.au>
2015-12-07 10:00:13 +0800

    fix typo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             string blah = "Hello World";
-            System.Console.Out.WriteLine(blah)
+            System.Console.Out.WriteLine(blah);
         }
     }
 }
